fe6488c2e9a9d0155cd749204995b850c587b9d9
Taylor Smith <taylorsmith.main@outlook.com>
2020-10-11 06:07:19 +0000

    added js modules

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+// Modules
 const http = require('http');
-var dt = require('./source/tools/dateTime');
+const lib = require('./source/tools/generic');
 
+// Config
 const hostname = 'localhost';
 const port = 3000;
 
+// Server Block
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end('Hello World! You suck! Minnie <3\n');
+  res.write("The date and time is currently:\n" + lib.myDateTime());
+  res.write(lib.myAwesomesauce());
+  res.end('\n\nHello World! Yep\n');
 });
 
+// Server Listen
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+
 });
